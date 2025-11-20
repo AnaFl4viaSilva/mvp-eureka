45e50c014d76e852912c9ecbda09de92d669a594
Vinicius Mendes <156609327+SwagMessiah02@users.noreply.github.com>
2025-11-20 10:15:26 -0300

    fix(User.js): Corrigindo conteudo
    Remove User model definition from User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,1 @@
-import mongoose from 'mongoose';
 
-const UserSchema = new mongoose.Schema({
-    nome: {type: String, required: true},
-    email: {type: String, required: true},
-    senha: {type: String, required: true}
-});
-
-const User = mongoose.model("User", UserSchema);
-
-export default User;
